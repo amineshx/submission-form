f7143c0cff7b885e90566902a45d2efe4b287881
Amine shx <98875453+amineshx@users.noreply.github.com>
2023-03-03 19:56:57 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var form= document.getElementById('sheetdb-form');
+form.addEventListener("submit",e =>{
+    e.preventDefault();
+    fetch(form.action,{
+        method:"post",
+        body: new FormData(document.getElementById('sheetdb-form')),
+    }).then(
+        Response => Response.json()
+    ).then((html)=>{
+        register.style.display = 'block';
+    });
+});
